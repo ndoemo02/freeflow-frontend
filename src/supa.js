01e535d91b43c37d1b2f9a7b775c50463ab050df
ndoemo02 <ndoemo02@gmail.com>
2025-09-13 13:30:04 +0200

    Update supa.js

--- src/supa.js
+++ src/supa.js
@@ -1,6 +1,7 @@
+// src/lib/supa.js
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  import.meta.env.VITE_SUPABASE_ANON_KEY // = SB_PUBLIC_ANON_KEY
 )
