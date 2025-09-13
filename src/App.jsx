9e4acbc4f75cc0a3f00df08aa9d2b24095a0fbc2
ndoemo02 <ndoemo02@gmail.com>
2025-09-13 13:56:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-import BusinessPanel from './pages/BusinessPanel'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { UserProvider } from "./UserContext";
+import CustomerPanel from "./pages/CustomerPanel.jsx";
+import BusinessPanel from "./pages/BusinessPanel.jsx";
 
-<Route path="/biz" element={<BusinessPanel />} />
+export default function App() {
+  return (
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/panel-klienta" element={<CustomerPanel />} />
+          <Route path="/panel-biznesu" element={<BusinessPanel />} />
+          <Route path="*" element={<Navigate to="/panel-klienta" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
+  );
+}
