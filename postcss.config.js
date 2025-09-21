a90a321ecababc66b7f487e5f209f342348af8d8
ndoemo02 <ndoemo02@gmail.com>
2025-09-21 07:07:16 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+// postcss.config.js  (ESM)
+import tailwind from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+  plugins: [tailwind, autoprefixer],
+}
