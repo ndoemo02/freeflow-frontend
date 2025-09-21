79e37c6d0ebf9104af6457199ad6f79dede984a4
ndoemo02 <ndoemo02@gmail.com>
2025-09-21 07:10:18 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js  (ESM)
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
